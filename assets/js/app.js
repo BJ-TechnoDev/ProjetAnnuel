cf1a10d68b14b785d1f818fe0b0c36fd30da1e9d
jeje-01350 <jeremyrossi1234@gmail.com>
2022-05-06 14:45:59 +0200

    bonne architecture

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,5 @@
 // any CSS you import will output into a single css file (app.scss in this case)
 import '../styles/app.scss';
 
-import AOS from 'aos';
-
 //lib js
-AOS.init();
-
 import '../bootstrap';
-
-
-
-
-
