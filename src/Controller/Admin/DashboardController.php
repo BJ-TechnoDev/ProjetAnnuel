ff621cf5420994671989ac7a467ef29e2f81f560
Sairkko <bastien.98@orange.fr>
2022-06-17 12:09:48 +0200

    FavIcon EasyAdmin + MenuItem::section EasyAdmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -25,15 +25,20 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Onyle');
+            ->setTitle('Onyle')
+            ->setFaviconPath('build/favicon 2.ico');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Accueil', 'fa fa-home');
+        yield MenuItem::section('Accès au site');
         yield MenuItem::linkToRoute("Accéder à votre site", 'fa fa-arrow-right-to-bracket', 'homepage');
+        yield MenuItem::section('Users');
         yield MenuItem::linkToCrud("Formateur", "fa fa-users", Intervenant::class);
+        yield MenuItem::section('Demande de Contrat');
         yield MenuItem::linkToCrud("Accéder à vos contrat", "fa fa-file-contract", Contrat::class);
+        yield MenuItem::section('Import / Export');
         yield MenuItem::linkToRoute("Import Contrat", "fa fa-file-arrow-down", '#');
         yield MenuItem::LinkToCrud("Import Maquette", 'fa fa-laptop-file', Intervenant::class);
         yield MenuItem::linkToRoute("Export en GoogleSheet", 'fa fa-file-arrow-up', '#');
