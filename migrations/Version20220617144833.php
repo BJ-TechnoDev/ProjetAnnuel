eaa502a2bc579dacfd88a3986b1bdb3046c6e713
jeje-01350 <jeremyrossi1234@gmail.com>
2022-06-17 16:50:27 +0200

    bdd maquette mise en place

--- migrations/Version20220617144833.php
+++ migrations/Version20220617144833.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220616113236 extends AbstractMigration
+final class Version20220617144833 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,13 @@ final class Version20220616113236 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE contrat DROP civilite');
+        $this->addSql('DROP TABLE matiere');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE contrat ADD civilite TINYINT(1) NOT NULL');
+        $this->addSql('CREATE TABLE matiere (id INT AUTO_INCREMENT NOT NULL, maquette_id INT NOT NULL, INDEX IDX_9014574A1D1D3C76 (maquette_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE matiere ADD CONSTRAINT FK_9014574A1D1D3C76 FOREIGN KEY (maquette_id) REFERENCES maquette (id)');
     }
 }
---
+++ src/Entity/Maquette.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MaquetteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MaquetteRepository::class)
+ */
+class Maquette
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
