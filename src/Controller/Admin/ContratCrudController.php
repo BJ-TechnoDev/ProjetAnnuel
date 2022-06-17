d59c7d8903358c279c98e059967d10cc77608ed3
Sairkko <bastien.98@orange.fr>
2022-06-17 14:09:06 +0200

    Update du Crud

--- src/Controller/Admin/ContratCrudController.php
+++ src/Controller/Admin/ContratCrudController.php
@@ -3,18 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Contrat;
-use App\Entity\Intervenant;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\FieldDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ContratCrudController extends AbstractCrudController
