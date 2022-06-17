4d1bb7b0e20841740f86a61a8e957a670fe207a3
Sairkko <bastien.98@orange.fr>
2022-06-17 11:16:48 +0200

    on ma dit de push

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -32,9 +32,11 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Accueil', 'fa fa-home');
         yield MenuItem::linkToRoute("Accéder à votre site", 'fa fa-arrow-right-to-bracket', 'homepage');
-        yield MenuItem::linkToCrud("Intervenant", "fa fa-users", Intervenant::class);
+        yield MenuItem::linkToCrud("Formateur", "fa fa-users", Intervenant::class);
         yield MenuItem::linkToCrud("Accéder à vos contrat", "fa fa-file-contract", Contrat::class);
-        yield MenuItem::linkToRoute("Import Contrat", "fa fa-file-import", '#');
+        yield MenuItem::linkToRoute("Import Contrat", "fa fa-file-arrow-down", '#');
+        yield MenuItem::LinkToCrud("Import Maquette", 'fa fa-laptop-file', Intervenant::class);
+        yield MenuItem::linkToRoute("Export en GoogleSheet", 'fa fa-file-arrow-up', '#');
 
     }
 
