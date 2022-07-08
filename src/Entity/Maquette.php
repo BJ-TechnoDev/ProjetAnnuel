570d2080b3701b134116709ccb1a09cee0ff2be6
jeje-01350 <jeremyrossi1234@gmail.com>
2022-07-08 13:31:00 +0200

    Base de données changer

--- src/Entity/Maquette.php
+++ src/Entity/Maquette.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\MaquetteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -18,11 +20,6 @@ class Maquette
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $class;
-
-    /**
      * @ORM\Column(type="integer")
      */
     private $volume_heure;
@@ -30,28 +27,27 @@ class Maquette
     /**
      * @ORM\Column(type="string", length=255)
      */
+    private $semestre;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=matiere::class, inversedBy="maquettes")
+     */
     private $matiere;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\ManyToMany(targetEntity=classe::class, inversedBy="maquettes")
      */
-    private $semestre;
+    private $classe;
+
+    public function __construct()
+    {
+        $this->matiere = new ArrayCollection();
+        $this->classe = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getClass(): ?string
-    {
-        return $this->class;
-    }
-
-    public function setClass(string $class): self
-    {
-        $this->class = $class;
-
-        return $this;
     }
 
     public function getVolumeHeure(): ?int
@@ -66,18 +62,6 @@ class Maquette
         return $this;
     }
 
-    public function getMatiere(): ?string
-    {
-        return $this->matiere;
-    }
-
-    public function setMatiere(string $matiere): self
-    {
-        $this->matiere = $matiere;
-
-        return $this;
-    }
-
     public function getSemestre(): ?string
     {
         return $this->semestre;
@@ -86,6 +70,84 @@ class Maquette
     public function setSemestre(string $semestre): self
     {
         $this->semestre = $semestre;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Contrat>
+     */
+    public function getContrats(): Collection
+    {
+        return $this->contrats;
+    }
+
+    public function addContrat(Contrat $contrat): self
+    {
+        if (!$this->contrats->contains($contrat)) {
+            $this->contrats[] = $contrat;
+            $contrat->setMaquette($this);
+        }
+
+        return $this;
+    }
+
+    public function removeContrat(Contrat $contrat): self
+    {
+        if ($this->contrats->removeElement($contrat)) {
+            // set the owning side to null (unless already changed)
+            if ($contrat->getMaquette() === $this) {
+                $contrat->setMaquette(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, matiere>
+     */
+    public function getMatiere(): Collection
+    {
+        return $this->matiere;
+    }
+
+    public function addMatiere(matiere $matiere): self
+    {
+        if (!$this->matiere->contains($matiere)) {
+            $this->matiere[] = $matiere;
+        }
+
+        return $this;
+    }
+
+    public function removeMatiere(matiere $matiere): self
+    {
+        $this->matiere->removeElement($matiere);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, classe>
+     */
+    public function getClasse(): Collection
+    {
+        return $this->classe;
+    }
+
+    public function addClasse(classe $classe): self
+    {
+        if (!$this->classe->contains($classe)) {
+            $this->classe[] = $classe;
+        }
+
+        return $this;
+    }
+
+    public function removeClasse(classe $classe): self
+    {
+        $this->classe->removeElement($classe);
 
         return $this;
     }
