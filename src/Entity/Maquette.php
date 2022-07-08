72adf15a11d0157979db845e78b86f7d62460a53
jeje-01350 <jeremyrossi1234@gmail.com>
2022-07-08 12:49:02 +0200

    maquette pedagogique + commencement de la doc dev

--- src/Entity/Maquette.php
+++ src/Entity/Maquette.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\MaquetteRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -19,8 +17,76 @@ class Maquette
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $class;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $volume_heure;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $matiere;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $semestre;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getClass(): ?string
+    {
+        return $this->class;
+    }
+
+    public function setClass(string $class): self
+    {
+        $this->class = $class;
+
+        return $this;
+    }
+
+    public function getVolumeHeure(): ?int
+    {
+        return $this->volume_heure;
+    }
+
+    public function setVolumeHeure(int $volume_heure): self
+    {
+        $this->volume_heure = $volume_heure;
+
+        return $this;
+    }
+
+    public function getMatiere(): ?string
+    {
+        return $this->matiere;
+    }
+
+    public function setMatiere(string $matiere): self
+    {
+        $this->matiere = $matiere;
+
+        return $this;
+    }
+
+    public function getSemestre(): ?string
+    {
+        return $this->semestre;
+    }
+
+    public function setSemestre(string $semestre): self
+    {
+        $this->semestre = $semestre;
+
+        return $this;
     }
 }
