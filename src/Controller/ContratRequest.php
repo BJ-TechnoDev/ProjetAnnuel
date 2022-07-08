94e43f612611948edc7e1e2afcead295591327ce
Sairkko <bastien.98@orange.fr>
2022-07-08 09:35:43 +0200

    update

--- src/Controller/ContratRequest.php
+++ src/Controller/ContratRequest.php
@@ -2,21 +2,12 @@
 
 namespace App\Controller;
 
-use App\Controller\Admin\ContratCrudController;
 use App\Entity\Contrat;
-use App\Service\ContratService;
 use App\Service\CsvService;
-use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Option\EA;
-use EasyCorp\Bundle\EasyAdminBundle\Factory\FilterFactory;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\CsvEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
+
 
 class ContratRequest
 {
@@ -24,7 +15,7 @@ class ContratRequest
     #[ParamConverter("contratRequest", class: Contrat::class)]
 
     public function contrat_request_export(Contrat $contrat, CsvService $csvService) {
-        $response = new Response($csvService->export());
+        $response = new Response($csvService->export($contrat, 'test'));
         $response->headers->set('Content-Type', 'text/csv');
         $response->headers->set('Content-Disposition', 'attachment');
 
